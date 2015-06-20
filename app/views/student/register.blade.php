53570fa08ac094e91fdd9973edfca4a8bd82b408
geekroot <narciso.arias21@gmail.com>
2015-06-20 17:51:11 -0400

    Design error fixed

--- app/views/student/register.blade.php
+++ app/views/student/register.blade.php
@@ -4,7 +4,7 @@
       <meta charset="UTF-8" />
 		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		  <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>TeamLand</title>
+      <title>DevsFarm</title>
 
       <script type="text/javascript" src="js/jquery-2.1.3.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
@@ -15,7 +15,7 @@
         $(document).ready(function() {
             $('#error_div').hide();
         });
-      </script> 
+      </script>
     </head>
 <body>
 
