5b7b156e2085087b1e9e73a59e204664fe74c149
Narciso Nunez(GeekRoot) <narciso.arias21@gmail.com>
2015-10-05 23:34:16 -0400

    Add teacher home page mockup

--- app/controllers/AssignmentController.php
+++ app/controllers/AssignmentController.php
@@ -218,4 +218,15 @@ class AssignmentController extends BaseController
 			return Response::json('00');
 		}
 	}
+
+    public static function getLatestAssignments()
+    {
+        $teacher = User::first(Auth::id());
+        $last_activity = $teacher->last_activity;
+        $teacher = Teacher::first(Auth::id());
+        $sections = $teacher->sections_id;
+        $assignments = Assignment::where('date_assigned', '>=', $last_activity)->get();
+
+        return $sections[0];
+    }
 }
